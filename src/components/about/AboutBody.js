f91b31258489e0d813264bb0a922292a52722580
Aidan Cusa <aidancusa@gmail.com>
2024-05-20 12:11:45 -0400

    Added cooper intro and relevant coursework

--- src/components/about/AboutBody.js
+++ src/components/about/AboutBody.js
@@ -3,6 +3,11 @@ import '../../styles/About.css';
 import FadeInSection from '../../tools/FadeInSection'
 
 const AboutBody = () => {
+
+    const handleNavigate = (url) => {
+        window.open(url, "_blank");
+    }
+
     return (
         <div>
             <br/>
@@ -18,7 +23,7 @@ const AboutBody = () => {
             <FadeInSection>
                 <div className="about-img-text-container">
                     <img src="about/family-photo-christmas.jpg" alt="Family Christmas" />
-                    <a> My name's Aidan, and I'm an upcoming Junior at The Cooper 
+                    <p> My name's Aidan, and I'm an upcoming Junior at The Cooper 
                         Union majoring in electrical engineering! I grew up in 
                         Monroe, Connecticut and attended Masuk High School where 
                         I was an active member of the VEX Robotics team 4478D.
@@ -29,7 +34,7 @@ const AboutBody = () => {
                         me exploring the streets and parks in New York City, belting 
                         lyrics in concerts, playing music and games with friends, 
                         and much more! 
-                    </a>
+                    </p>
                 </div>
             </FadeInSection>
             <br/> <br/> <br/> <br/>
@@ -45,20 +50,68 @@ const AboutBody = () => {
             <br/>
             <FadeInSection>
                 <div className="about-img-text-container">
-                    <img src="about/family-photo-christmas.jpg" alt="Family Christmas" />
-                    <a> My name's Aidan, and I'm an upcoming Junior at The Cooper 
-                        Union majoring in electrical engineering! I grew up in 
-                        Monroe, Connecticut and attended Masuk High School where 
-                        I was an active member of the VEX Robotics team 4478D.
-                        Currently, I'm interested in Deep Learning Engineering,
-                        focusing on taking classes to aid this passion.
-                        <br/><br/>
-                        When not involved in academics, you could probably find 
-                        me exploring the streets and parks in New York City, belting 
-                        lyrics in concerts, playing music and games with friends, 
-                        and much more! 
-                    </a>
+                    <p> The Cooper Union for the Advancement of Science and Art (The Cooper
+                        Union) is a private institute located within the East Village in New 
+                        York City. The school was founded in 1859 by Peter Cooper and offers
+                        degrees in engineering, art, and architecture. 
+
+                        <a className = "about-link" onClick={() => handleNavigate('https://cooper.edu/admissions/facts')}> 
+                            Click here to learn more about The Cooper Union! 
+                        </a> 
+
+                        <br/> <br/> 
+                        At The Cooper Union, I've learned to enjoy the world of 
+                        electrical engineering more than I ever thought I would. 
+                        I currently have a major GPA of 4.00, an overall GPA of 3.7, 
+                        and I am on a track to graduate with a minor in computer 
+                        science. Although I have only been a student for two years,
+                        I have made countless memories with my friends here and 
+                        appreciate what it has given me thus far.
+                    </p>
+                    <img src="about/chemlab-with-noam.jpg" alt="Chem Lab with Noam" />
                 </div> 
+            </FadeInSection>
+
+            <br/> <br/>
+            <FadeInSection>
+                <div class="rc-table">
+                    <table>
+                        <tr>
+                            <td>
+                                <h2> Relevant Coursework <br/> Includes: </h2>
+                            </td>
+                            <td>
+                                <h3>Sophomore Year</h3>
+                                <ul>
+                                    <li>Software Engineering</li>
+                                    <li>Computer Architecture</li>
+                                    <li>Data Structures and Algorithms II</li>
+                                    <li>Circuit Analysis</li>
+                                    <li>Signal Processing</li>
+                                    <li>Electronics I</li>
+                                    <li>Vector Calculus</li>
+                                    <li>Partial & Ordinary Differential Equations</li>
+                                    <li>Probability</li>
+                                    <li>Physics II & III</li>
+                                </ul>
+                            </td>
+                            <td>
+                                <h3>Freshman Year</h3>
+                                <ul>
+                                    <li>Programming for Electrical Engineers</li>
+                                    <li>Data Structures and Algorithms I</li>
+                                    <li>Digital Logic Design</li>
+                                    <li>Calculus I & II</li>
+                                    <li>Introduction to Linear Algebra</li>
+                                    <li>Physics Mechanics</li>
+                                    <li>General Chemistry</li>
+                                    <li>Engineering Design & Problem Solving</li>
+                                    <li>Engineering Graphics (Drafting)</li>
+                                </ul>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </FadeInSection>
         </div>
     );
