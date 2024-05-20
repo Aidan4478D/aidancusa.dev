9c2cb8e639cd5d81db062596413114a1eed6a637
Aidan Cusa <aidancusa@gmail.com>
2024-05-20 16:57:59 -0400

    Added robotics content to about page

--- src/components/about/AboutBody.js
+++ src/components/about/AboutBody.js
@@ -41,7 +41,7 @@ const AboutBody = () => {
             
             <hr/>
             <div className="image-wide-container">
-                <img src="about/cooper_wide.jpg" alt="The Cooper Union" className="wide-image" />
+                <img src="about/cooper-wide.jpg" alt="The Cooper Union" className="wide-image" />
                 <div className="centered-text">
                     <h2>The Cooper Union</h2>
                 </div>
@@ -59,6 +59,8 @@ const AboutBody = () => {
                         <a className = "about-link" onClick={() => handleNavigate('https://cooper.edu/admissions/facts')}> 
                             Click here to learn more about The Cooper Union! 
                         </a> 
+
+                        &nbsp;
 
                         <br/> <br/> 
                         At The Cooper Union, I've learned to enjoy the world of 
@@ -156,7 +158,137 @@ const AboutBody = () => {
                 </div>
             </div>
             <hr/>
+            
+            <br/><br/><br/>
 
+            <FadeInSection>
+                <div className="about-img-text-container Light">
+                    <p> 
+                        I joined my High School's VEX Robotics program in my Freshman 
+                        year of High School and formed a team with a couple of my close 
+                        friends. The VEX Robotics Compeittion (VRC) is the largest and 
+                        fastest growing middle school and high school robotics program 
+                        globally with more than 20,000 teams from 50 countries playing 
+                        in over 1,700 competitions worldwide. Each year, an engineering 
+                        challenge is presented in the form of a game where the students 
+                        build innovative robots and compete year-round. &nbsp;
+
+                        <a className = "about-link" onClick={() => handleNavigate('https://roboticseducation.org/vex-robotics-competition/')}> 
+                            To learn more about VEX Robotics or this year's game, click here! 
+                        </a>
+
+                        &nbsp;
+                        <br/><br/>
+
+                        On my team, I took on the role as the programmer as it was the 
+                        only vacant slot and we became team 4478D.
+                    </p>
+                </div>
+                <div className="about-img-text-container Light">
+                    <img src="about/robotics/team-photo-2019.jpg" alt="2019 Robotics Team" />
+                    <p>
+                        We quickly found our stride, winning the first three 
+                        tournaments we attended, and placing as finalists in 
+                        another four. We made it to the semi-finals of the
+                        Southern New England Regional Compeititon, and found 
+                        our way to the World Championships at the end of the year 
+                        where we placed 28/93 in our division. 
+                    </p>
+                    <img src="about/robotics/worlds-screen-2019.jpg" alt="2019 Robotics Worlds" />
+                </div>
+            </FadeInSection>
+            
+            <FadeInSection>
+                <div className="about-img-text-container Dark">
+                    <p>
+                        In the next years (2019-2020) season, our team went on to win five
+                        tournaments, one of them being a national championship.
+                        Additionally, we set the world record for the highest 
+                        scoring 15-second autonomous mode. We qualified to go to
+                        the world championships but were ultimately unable to 
+                        participate in the event as well as the regional championships
+                        due to the COVID-19 pandemic. 
+                    </p>
+                    <img src="about/robotics/natm-champs-2020.jpg" alt="2020 NATM Champs" />
+                </div>
+                
+                <div className="about-img-text-container Dark">
+                    <img src="about/robotics/natm-match-2020.jpg" alt="2020 NATM Match" />
+
+                    <a> <iframe width="444" height="250" src="https://www.youtube.com/embed/bhACJ6sQ1go?si=oaN63Rzyedrkt1Q3" 
+                        title="YouTube video player" frameborder="0" allow="accelerometer; 
+                        autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                    </iframe> </a>
+
+                    <img src="about/robotics/team-photo-2020.JPG" alt="2020 Team Photo" />
+                </div>
+            </FadeInSection>
+
+            <FadeInSection>
+                <div className="about-img-text-container Light">
+                    <p>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/L9lCQwYtMQg?si=MC7VH6EwKfJE7LAn" 
+                            title="YouTube video player" frameborder="0" allow="accelerometer; 
+                            autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                        </iframe>
+                    </p>
+                    <p>
+                        The 2020-2021 season's game occured during the pandemic, where
+                        tournaments were almost all remote and in-person contact was 
+                        limited. Despite these challenges, we were still able to set
+                        the 3rd highest skills score in the world and 1st in the
+                        United States in the early season.
+
+                        <br/><br/>
+                        
+                        We participated in one in-person competition and one fully
+                        remote competition where we were able to become tournament
+                        champions in both settings. 
+                    </p>
+                </div>
+            </FadeInSection>
+            <FadeInSection>
+                <div className="about-img-text-container Dark">
+                     <p>
+                        Finally in our senior year during the 2021-2022 season, 
+                        we were able to rank first in 6/10 tournaments we attended, 
+                        going undefeated at both the Southern New England Regional 
+                        Championships and the VEX Robotics World Championships during 
+                        the qualifying rounds.
+
+                        <br/><br/>
+
+                        Our team was able to win the Southern New England Regional
+                        Championships and accumualte other awards at the event such
+                        as the skills challenge champions and the amaze award for
+                        having the highest consistently scoring robot. 
+
+                    </p>
+                    <img src="about/robotics/regionals-team-photo-2022.jpg" alt="2020 NATM Champs" />
+                    <img src="about/robotics/rank-1-2022.jpg" alt="2020 NATM Champs" />
+
+                </div>
+                <div className="about-img-text-container Dark">
+                    <img src="about/robotics/worlds-team-photo-2022.jpg" alt="2020 NATM Champs" />
+                    <p>
+                        We were able to travel to San Fransisco, CA, and Dallas, TX, to
+                        compete with top tier teams going far in each tournament. 
+                        
+                        <br/><br/>
+
+                        At the World Championships that year, we ranked second in our
+                        divison, being one of only five teams in the competition to
+                        go undefeated in the qualifying rounds. Although we ultimately 
+                        lost in the semi-finals, our team was awarded the Inspire Award, 
+                        an award given to the team that "will have a clear vision 
+                        for their future and will participate with both a high level 
+                        of integrity and good sportsmanship, believing they can achieve 
+                        what they set out to achieve through their diligence"
+                    </p>
+                </div>
+            </FadeInSection>
         </div>
     );
 }
